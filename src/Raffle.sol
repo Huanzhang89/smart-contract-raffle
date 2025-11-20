6d3d7f9873d95115171b0f8f0e89fe55f080cba1
Huan Zhang <huanzhang30@gmail.com>
2025-11-20 05:37:17 +0000

    enter raffle with events

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -30,12 +30,32 @@ pragma solidity 0.8.19;
  * @dev Implements ChainLink VRFv2.5
  */
 contract Raffle {
+    /**
+     * Errors
+     */
+    error Raffle__SendMoreToEnterRaffle();
+
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /**
+     * Events
+     */
+    event RaffleEntered(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
-    function enterRaffle() public payable {}
+
+    function enterRaffle() public payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent");
+        if (msg.value <= i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender);
+    }
 
     function pickWinner() public {}
 
